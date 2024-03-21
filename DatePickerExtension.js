38592ebbdf80bdf38765229cf3c766d71f589fd0
Convizr <162603628+Convizr@users.noreply.github.com>
2024-03-21 20:51:52 +0100

    Update DatePickerExtension.js

--- DatePickerExtension.js
+++ DatePickerExtension.js
@@ -1,42 +1,43 @@
 export const DatePickerExtension = {
-    name: 'DatePicker',
-    type: 'response',
-    match: ({ trace }) =>
-      trace.type === 'ext_date_picker' || trace.payload.name === 'ext_date_picker',
-    render: ({ trace, element }) => {
-      const datePickerContainer = document.createElement('div');
-  
-      datePickerContainer.innerHTML = `
-            <style>
-              .calendar {
-                border: 1px solid #ccc;
-                border-radius: 5px;
-                padding: 10px;
-              }
-              input[type="date"] {
-                width: 100%;
-                padding: 10px;
-                margin: 5px 0;
-                border-radius: 5px;
-                border: 1px solid #ccc;
-                font-size: 16px;
-              }
-            </style>
-            <div class="calendar">
-              <input type="date" id="datePicker" name="datePicker">
-            </div>
-          `;
-  
-      const input = datePickerContainer.querySelector('#datePicker');
-      input.addEventListener('change', function (event) {
-        console.log('Date selected:', event.target.value);
-        window.voiceflow.chat.interact({
-          type: 'complete',
-          payload: { date: event.target.value },
-        });
+  name: 'DatePicker',
+  type: 'response',
+  match: ({ trace }) =>
+    trace.type === 'ext_date_picker' || trace.payload.name === 'ext_date_picker',
+  render: ({ trace, element }) => {
+    const datePickerContainer = document.createElement('div');
+    const today = new Date().toISOString().split('T')[0];
+
+    datePickerContainer.innerHTML = `
+          <style>
+            .calendar {
+              border: 1px solid #ccc;
+              border-radius: 5px;
+              padding: 10px;
+            }
+            input[type="date"] {
+              width: 100%;
+              padding: 10px;
+              margin: 5px 0;
+              border-radius: 5px;
+              border: 1px solid #ccc;
+              font-size: 16px;
+            }
+          </style>
+          <div class="calendar">
+            <input type="date" id="datePicker" name="datePicker" min="${today}">
+          </div>
+        `;
+
+    const input = datePickerContainer.querySelector('#datePicker');
+    input.addEventListener('change', function (event) {
+      console.log('Date selected:', event.target.value);
+      window.voiceflow.chat.interact({
+        type: 'complete',
+        payload: { date: event.target.value },
       });
-  
-      element.appendChild(datePickerContainer);
-      console.log('DatePicker rendered');
-    },
-  };  
+    });
+
+    element.appendChild(datePickerContainer);
+    console.log('DatePicker rendered with future dates only');
+  },
+};
